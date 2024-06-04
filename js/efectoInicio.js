ff32277292a12fbe16fed60f492a4bed35fe235a
FranCappe <francappe13@gmail.com>
2024-06-04 13:22:13 -0300

    Cambios

--- js/efectoInicio.js
+++ js/efectoInicio.js
@@ -17,24 +17,4 @@ function irArriba() {
 }
 
 
-const imageWrappers = document.querySelectorAll('.image-wrapper');
 
-function showImages() {
-  imageWrappers.forEach((imageWrapper) => {
-    if (isElementInViewport(imageWrapper)) {
-      imageWrapper.classList.add('show');
-    }
-  });
-}
-
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-window.addEventListener('scroll', showImages);
